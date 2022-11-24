2fdad71ad0fd42756cd1abb9d30510d84d5a1b06
Sushil Ghate <sushilsg30@gmail.com>
2022-11-24 12:23:47 +0530

    block-BJaake

--- block-BJaake/code/index.js
+++ block-BJaake/code/index.js
@@ -1,45 +1,61 @@
-let url = fetch(`https://api.spaceflightnewsapi.net/v3/articles?_limit=30`);
+let url = `https://api.spaceflightnewsapi.net/v3/articles?_limit=30`;
 
 let newsElm = document.querySelector('.news');
+let select = document.querySelector('select');
+let allNews = [];
 
 function renderNews(news) {
+    newsElm.innerHTML = '';
     news.forEach(newsItem => {
-        console.log(newsItem.title);
         let li = document.createElement('li');
+        li.classList = 'flex';
         let img  = document.createElement("img");
+        img.classList = "article-width";
         img.src = newsItem.imageUrl;
         img.alt = newsItem.title; 
         let div = document.createElement("div");
+        div.classList = "article-width";
         let span = document.createElement("span");
         span.innerText = newsItem.newsSite;
+        span.classList = "source";
         let h3 = document.createElement("h3");
         h3.innerText = newsItem.title;
         let a = document.createElement('a');
         a.href = newsItem.url;
-        a.append(button); 
         let button = document.createElement("button");
         button.innerText = 'Read More';
-        div.append(span, h3, button);
+        a.append(button); 
+        div.append(span, h3, a);
         li.append(img, div);
         newsElm.append(li);
     });
 }
-
-function fetch(url) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = function (){
-         let userData = JSON.parse(xhr.response);
-         console.log(userData);
-    }
-    xhr.onerror = function () {
-        console.log('Something went Wrong!');
-    };
-    xhr.send();
+function displayOptions(sources) {
+    sources.forEach(source => {
+        let option = document.createElement("option");
+        option.innerText = source;
+        option.value = source
+        select.append(option)
+    })
 }
 
 fetch(url).then((resolve) => resolve.json())
     .then((news) => {
-        console.log(news);
+        allNews = news;
         renderNews(news);
+        let allSources = Array.from(new Set(news.map((n) => n.newsSite)));
+        displayOptions(allSources);
     });
+
+
+select.addEventListener('change', (event) => {
+    let source = event.target.value.trim();
+    if(source) {
+            var filteredNews = allNews.filter(
+              (news) => news.newsSite === source
+            );
+    } else {
+        filteredNews = allNews;
+    }
+    renderNews(filteredNews);
+})
